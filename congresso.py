b50a194259b8c392d4e78fd7e1ea29d05088c74d
elisamanoeli <140274690+elisamanoeli@users.noreply.github.com>
2024-08-27 21:43:53 -0300

    Add files via upload

--- congresso.py
+++ congresso.py
@@ -240,35 +240,98 @@ if st.session_state["botao_clicado"] and st.session_state["opcao_escolhida"] == 
             st.error("Por favor, preencha todos os campos.")
 
         if st.session_state["formulario_preenchido"]:
-            if st.session_state["botao_clicado"] == "adimplente":
-                st.markdown("""
-                    <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
-                        <div style="text-align:center; color:#0B0C45;">
-                            <p>INSCRIÇÃO EFETUADA COM SUCESSO</p>
-                            <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
-                            <p>30 DE NOVEMBRO 7:30</p>
-                            <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
-                            <p>Churrasco de Confraternização</p>
-                            <p>30 DE NOVEMBRO 13:30</p>
-                        </div>
-                    </div>
-                """, unsafe_allow_html=True)
-            elif st.session_state["botao_clicado"] == "em_negociacao":
-                st.markdown("""
-                    <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
-                        <div style="text-align:center; color:#0B0C45;">
-                            <p>SUA INSCRIÇÃO SERÁ EFETIVADA APÓS O PAGAMENTO DE 50%</p>
-                            <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
-                            <p>30 DE NOVEMBRO 7:30</p>
-                            <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
-                            <p>Churrasco de Confraternização</p>
-                            <p>30 DE NOVEMBRO 13:30</p>
-                            <p>Local do churrasco a definir, Curitiba/PR</p>
-                            <p><strong>PIX CNPJ: 39.486.619/0001-93</strong></p>
-                            <p><strong>VALOR: R$ 00,00</strong></p>
-                        </div>
-                    </div>
-                """, unsafe_allow_html=True)
+    if st.session_state["botao_clicado"] == "adimplente":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>INSCRIÇÃO EFETUADA COM SUCESSO</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    elif st.session_state["botao_clicado"] == "em_negociacao":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>SUA INSCRIÇÃO SERÁ EFETIVADA APÓS O PAGAMENTO DE 50%</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                    <p>Local do churrasco a definir, Curitiba/PR</p>
+                    <p><strong>PIX CNPJ: 39.486.619/0001-93</strong></p>
+                    <p><strong>VALOR: R$ 00,00</strong></p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    elif st.session_state["botao_clicado"] == "mensalidade_atrasada":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>SUA INSCRIÇÃO SERÁ EFETIVADA APÓS O PAGAMENTO DO VALOR TOTAL</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                    <p>Local do churrasco a definir, Curitiba/PR</p>
+                    <p><strong>PIX CNPJ: 39.486.619/0001-93</strong></p>
+                    <p><strong>VALOR: R$ 00,00</strong></p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+if st.session_state["formulario_preenchido"]:
+    if st.session_state["botao_clicado"] == "adimplente":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>INSCRIÇÃO EFETUADA COM SUCESSO</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    elif st.session_state["botao_clicado"] == "em_negociacao":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>SUA INSCRIÇÃO SERÁ EFETIVADA APÓS O PAGAMENTO DE 50%</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                    <p>Local do churrasco a definir, Curitiba/PR</p>
+                    <p><strong>PIX CNPJ: 39.486.619/0001-93</strong></p>
+                    <p><strong>VALOR: R$ 00,00</strong></p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    elif st.session_state["botao_clicado"] == "mensalidade_atrasada":
+        st.markdown("""
+            <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
+                <div style="text-align:center; color:#0B0C45;">
+                    <p>SUA INSCRIÇÃO SERÁ EFETIVADA APÓS O PAGAMENTO DO VALOR TOTAL</p>
+                    <p>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</p>
+                    <p>30 DE NOVEMBRO 7:30</p>
+                    <p>Rua Barão do Rio Branco, 370 - Centro, Curitiba/PR</p>
+                    <p>Churrasco de Confraternização</p>
+                    <p>30 DE NOVEMBRO 13:30</p>
+                    <p>Local do churrasco a definir, Curitiba/PR</p>
+                    <p><strong>PIX CNPJ: 39.486.619/0001-93</strong></p>
+                    <p><strong>VALOR: R$ 00,00</strong></p>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+
                         elif st.session_state["botao_clicado"] == "mensalidade_atrasada":
                 st.markdown("""
                     <div class="success-box" style="background-color:#FFFFFF; border:2px solid #0B0C45; border-radius:10px; padding:20px; margin-top:20px;">
