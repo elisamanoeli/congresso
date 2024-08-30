589c359a455561239e7a7385ee47f53de9b3ec6c
elisamanoeli <140274690+elisamanoeli@users.noreply.github.com>
2024-08-30 15:41:33 -0300

    Add files via upload

--- congresso.py
+++ congresso.py
@@ -8,6 +8,7 @@ import gspread
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import streamlit.components.v1 as components
 
 # Verifica se as credenciais do GCP estão no st.secrets
 if "gcp_service_account" in st.secrets:
@@ -107,6 +108,13 @@ if "gcp_service_account" in st.secrets:
 else:
     st.error("Não foi possível carregar as credenciais do GCP. A integração com o Google Sheets não está disponível.")
 
+# Adicionando JavaScript para rolar a página
+scroll_script = """
+<script>
+    window.scrollTo(0,document.body.scrollHeight);
+</script>
+"""
+
 # O código para a interface do usuário continua...
 
 # CSS personalizado para ocultar a barra superior do Streamlit e remover o padding superior
@@ -199,7 +207,20 @@ st.markdown(
 )
 
 # Exibe o layout dos botões centrados
-st.image("logo.png", width=200)
+# CSS personalizado para garantir que o tamanho da imagem seja controlado
+st.markdown(
+    """
+    <style>
+    .stImage img {
+        width: 200px !important;
+        height: auto !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Exibe o logo com largura fixa de 200px
+st.image("logo.png", use_column_width=False, width=200)
 st.markdown("<h1 style='text-align: center;'>I Congresso de Papiloscopia da ASIIP - Comparação Facial Humana</h1>", unsafe_allow_html=True)
 
 st.write("Escolha uma opção para prosseguir com a inscrição:")
@@ -249,8 +270,8 @@ if st.session_state.get("botao_clicado") and st.session_state.get("opcao_escolhi
     email = st.text_input("Email", key="input_email_associado")
     telefone = st.text_input("Telefone", key="input_telefone_associado")
 
-    # Força a rolagem para o final após exibir o formulário
-    st.experimental_rerun()
+    # Injetando o JavaScript para rolar a página automaticamente após carregar o formulário
+    components.html(scroll_script)
 
     if st.button("ENVIAR", key="btn_enviar_associado"):
         if nome_completo and email and telefone:
@@ -343,7 +364,7 @@ if st.session_state.get("instituicao_selecionada") and st.session_state.get("opc
     telefone_na = st.text_input("Telefone", key="input_telefone_na")
 
     if st.button("ENVIAR (NÃO ASSOCIADO)", key="btn_enviar_nao_associado"):
-        if nome_completo_na and email_na and telefone_na:
+        if nome_completo_na and email_na e telefone_na:
             if not email_valido(email_na):
                 st.error("Por favor, insira um email válido.")
             elif not telefone_valido(telefone_na):
